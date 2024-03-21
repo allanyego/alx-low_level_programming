4ba1aa94aaadda901ae3d84c34c24f3b950feacc
allanyego <allanyego05@gmail.com>
2024-03-21 23:15:48 +0300

    Implement optional tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - The program entry
+ *
+ * Return: the exit code
+ */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
