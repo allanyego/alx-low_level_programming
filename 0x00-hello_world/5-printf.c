9f9e4ace5e7317f8fa735dabd84cb8ea3051afe0
allanyego <allanyego05@gmail.com>
2024-03-21 14:58:06 +0300

    Fix buggy scripts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ * main - main program
+ */
+int main(void)
+{
   printf("with proper grammar, but the outcome is a piece of art,\n");
-  return 0;
+  return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ * main - main program
+ */
+int main(void)
+{
   char c;
   int i;
   long l;
   long long ll;
   float f;
-  
+
   printf("Size of a char: %d byte(s)\n", (int)sizeof(c));
-  printf("Size of a int: %d byte(s)\n", (int)sizeof(i));
-  printf("Size of a long: %d byte(s)\n", (int)sizeof(l));
-  printf("Size of a long long: %d byte(s)\n", (int)sizeof(ll));
+  printf("Size of an int: %d byte(s)\n", (int)sizeof(i));
+  printf("Size of a long int: %d byte(s)\n", (int)sizeof(l));
+  printf("Size of a long long int: %d byte(s)\n", (int)sizeof(ll));
   printf("Size of a float: %d byte(s)\n", (int)sizeof(f));
-  return 0;
+  return (0);
 }
